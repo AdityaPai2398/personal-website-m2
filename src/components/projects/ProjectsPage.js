f0f338c9d088ed207771cfb762b59e0f477b54d8
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-12-05 01:51:40 -0500

    Changed ProjectsDisplay to react-transition-group

--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-children-prop */
 import React from 'react';
-import {Route/*, Switch*/} from 'react-router-dom';
-import PageAnimationWrapper from '../common/PageAnimationWrapper';
+import {Route, Switch} from 'react-router-dom';
+// import PageAnimationWrapper from '../common/PageAnimationWrapper';
 import ProjectsDisplay from './ProjectsDisplay'
 import ProjZR from './project-pages/ProjZR';
 import ProjFindR from './project-pages/ProjFindR';
@@ -11,6 +11,7 @@ import ProjCLHSOrch from './project-pages/ProjCLHSOrch';
 import ProjPolymap from './project-pages/ProjPolymap';
 import ProjPersonalWebsite from './project-pages/ProjPersonalWebsite';
 import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class ProjectsPage extends React.Component {
   render() {
@@ -31,7 +32,21 @@ class ProjectsPage extends React.Component {
           <h1><span>Projects</span></h1>
         </div>
       </div> */}
-      <Route exact path="/projects" children={({ match, ...rest }) => (
+      <TransitionGroup>
+      <CSSTransition key={this.props.location.key} classNames='fade' timeout={240}>
+        <Switch location={this.props.location}>
+          <Route exact path="/projects" component={ProjectsDisplay}/>
+          <Route exact path="/projects/zerorobotics" component={ProjZR}/>
+          <Route exact path="/projects/findr" component={ProjFindR}/>
+          <Route exact path="/projects/recyclo" component={ProjRecyclo}/>
+          <Route exact path="/projects/moveover" component={ProjMoveOver}/>
+          <Route exact path="/projects/clhsorchestra" component={ProjCLHSOrch}/>
+          <Route exact path="/projects/polymap" component={ProjPolymap}/>
+          <Route exact path="/projects/personalwebsite" component={ProjPersonalWebsite}/>
+        </Switch>
+      </CSSTransition>
+      </TransitionGroup>
+      {/* <Route exact path="/projects" children={({ match, ...rest }) => (
         <PageAnimationWrapper mounted={match} home={false} page="Projects">
           <ProjectsDisplay {...rest}/>
         </PageAnimationWrapper>
@@ -70,7 +85,7 @@ class ProjectsPage extends React.Component {
         <PageAnimationWrapper mounted={match} home={false} page="Personal Website | Projects">
           <ProjPersonalWebsite {...rest}/>
         </PageAnimationWrapper>
-      )}/>
+      )}/> */}
     </section>
     </div>
     )
@@ -83,7 +98,8 @@ class ProjectsPage extends React.Component {
 
 ProjectsPage.propTypes = {
   // setHome: PropTypes.func.isRequired,
-  unsetHome: PropTypes.func.isRequired
+  unsetHome: PropTypes.func.isRequired,
+  location: PropTypes.object
 };
 
 
